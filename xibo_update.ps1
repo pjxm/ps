28b03fdd5e3a64e1222cb9a3dafab446db02109d
pjxm <53785801+pjxm@users.noreply.github.com>
2019-08-06 12:39:06 +0100

    added xibo update script
    Silently downloads and updates xibo, then kills and restarts the process, hopefully seamlessly

---
+++ xibo_update.ps1
@@ -0,0 +1,13 @@
+﻿$uri = 'https://xibo.org.uk/api/downloads/windows'
+$path = 'C:\xibo\latestInstaller.msi'
+
+# Set TLS level to 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Download and install latest player
+Invoke-WebRequest -Uri $uri -OutFile $path
+msiexec -package $path -quiet
+
+# Stop current xibo processes and start new player
+Stop-Process -Name XiboClient* -Force
+Start-Process 'C:\Program Files (x86)\Xibo Player\XiboClient.exe'
